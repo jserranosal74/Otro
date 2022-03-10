47c67ab7d20353b389cefaf9cb6455da58d4c0f3
Jesus Serrano <jserranosal@gmail.com>
2022-03-10 08:24:40 -0600

    nuevo

---
+++ src/app/Models/Propiedad.model.ts
@@ -0,0 +1,11 @@
+export class Propiedad {
+    constructor(
+      public Id_Propiedad: number,
+      public Clave: string,
+      public Descripcion: string,
+      public Fecha_Alta: string,
+      public Fecha_Modificacion: string,
+      public Id_Usuario: string,
+      public Id_Estatus: string 
+    ) {}
+  }
