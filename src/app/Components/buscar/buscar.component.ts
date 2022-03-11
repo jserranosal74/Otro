8f42a6a9830b998e15697e16473928a20c3d179f
Jesus Serrano <jserranosal@gmail.com>
2022-03-11 11:18:55 -0600

    Se completa componente de busqueda

--- src/app/Components/buscar/buscar.component.ts
+++ src/app/Components/buscar/buscar.component.ts
@@ -15,31 +15,38 @@ export class BuscarComponent implements OnInit {
 
     this._activatedRoute.params.subscribe((routeParams) => {
       //console.log('entro a constructor: '+this._activatedRoute.snapshot.params['tipo']);
-      Swal.fire({
-        title: '¿Desea guardar los cambios?' + this._activatedRoute.snapshot.params['tipo'],
-        showDenyButton: true,
-        showCloseButton: true,
-        showCancelButton: false,
-        confirmButtonText: 'Si',
-        denyButtonText: `No`,
-        footer: '<a href="">¿Por que estas viendo esta falla?</a>'
-      }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-          Swal.fire('Los cambios se guardaron de manera correcta', '', 'success')
-        } else if (result.isDenied) {
-          Swal.fire('La información no ha sido guardada', '', 'info')
-        }
-      })
+
+      // Swal.fire({
+      //   title: '¿Desea guardar los cambios?' + this._activatedRoute.snapshot.params['tipo'],
+      //   showDenyButton: true,
+      //   showCloseButton: true,
+      //   showCancelButton: false,
+      //   confirmButtonText: 'Si',
+      //   denyButtonText: `No`,
+      //   footer: '<a href="">¿Por que estas viendo esta falla?</a>'
+      // }).then((result) => {
+      //   /* Read more about isConfirmed, isDenied below */
+      //   if (result.isConfirmed) {
+      //     Swal.fire('Los cambios se guardaron de manera correcta', '', 'success')
+      //   } else if (result.isDenied) {
+      //     Swal.fire('La información no ha sido guardada', '', 'info')
+      //   }
+      // })
     });
 
   }
 
   ngOnInit(): void {
+  }
 
-    
-
-    
-
+  buscarPropiedades(){
+    Swal.fire({
+      icon: 'success',
+      title: 'Buscando....',
+      text: 'Espere un momento por favor',
+      showCancelButton:true,
+      showDenyButton:false,
+      footer: '<a href="">Why do I have this issue?</a>'
+    })
   }
 }
